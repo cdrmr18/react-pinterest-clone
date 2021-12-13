8251049a95266412d455b1e77ba8195187260f69
Kathy Tavia <kathy.tavia@gmail.com>
2021-12-13 21:22:43 +0100

    set up header layout for navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,37 @@
 import React from 'react';
+import '../style/Header.css';
 
 const Header = () => {
-  return <></>;
+  return (
+    <div className="app_header">
+      <div className="header_wrapper">
+        <div className="header_logo">
+          <p>logo</p>
+        </div>
+        <div className="header_button" homePage>
+          <p>homepage</p>
+        </div>
+        <div className="header_button" following>
+          <p>following</p>
+        </div>
+        <div className="header_search">
+          <div className="header_searchContainer">
+            <p>search icon</p>
+            <form>
+              <input type="search" />
+              <button>'Submit</button>
+            </form>
+          </div>
+        </div>
+        <div className="header_menuItems">
+          <p>icon</p>
+          <p>icon</p>
+          <p>icon</p>
+          <p>icon</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
