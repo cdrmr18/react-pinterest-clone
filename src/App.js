8a1f0473b4648096f989c695fdf75cf0431bf840
Kathy Tavia <kathy.tavia@gmail.com>
2021-12-13 22:00:13 +0100

    added materialUI & navbar icons

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import Header from './components/Header';
 function App() {
   return (
     <div className="App">
-      <Header />
+      <div className="app_header">
+        <Header />
+      </div>
+      <div className="app_body">{/* <MainBoard /> */}</div>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,34 +1,51 @@
 import React from 'react';
+import PinterestIcon from '@mui/icons-material/Pinterest';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import SearchIcon from '@mui/icons-material/Search';
+import TextsmsIcon from '@mui/icons-material/Textsms';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import { IconButton } from '@mui/material';
 import '../style/Header.css';
 
 const Header = () => {
   return (
-    <div className="app_header">
-      <div className="header_wrapper">
-        <div className="header_logo">
-          <p>logo</p>
+    <div className="header_wrapper">
+      <div className="header_logo">
+        <IconButton>
+          <PinterestIcon />
+        </IconButton>
+      </div>
+      <div className="header_button" homePage>
+        <p>homepage</p>
+      </div>
+      <div className="header_button" following>
+        <p>following</p>
+      </div>
+      <div className="header_search">
+        <div className="header_searchContainer">
+          <IconButton>
+            <SearchIcon />
+          </IconButton>
+          <form>
+            <input type="search" />
+            <button>Submit</button>
+          </form>
         </div>
-        <div className="header_button" homePage>
-          <p>homepage</p>
-        </div>
-        <div className="header_button" following>
-          <p>following</p>
-        </div>
-        <div className="header_search">
-          <div className="header_searchContainer">
-            <p>search icon</p>
-            <form>
-              <input type="search" />
-              <button>'Submit</button>
-            </form>
-          </div>
-        </div>
-        <div className="header_menuItems">
-          <p>icon</p>
-          <p>icon</p>
-          <p>icon</p>
-          <p>icon</p>
-        </div>
+      </div>
+      <div className="header_menuItems">
+        <IconButton>
+          <NotificationsIcon />
+        </IconButton>
+        <IconButton>
+          <TextsmsIcon />
+        </IconButton>
+        <IconButton>
+          <AccountCircleIcon />
+        </IconButton>
+        <IconButton>
+          <KeyboardArrowDownIcon />
+        </IconButton>
       </div>
     </div>
   );
