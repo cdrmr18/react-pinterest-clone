a3a40dabd5d7672b637a9fd07e758baabe5ce840
Kathy Tavia <kathy.tavia@gmail.com>
2021-12-14 22:58:46 +0100

    add: axios for GET photos from unsplash

--- src/App.js
+++ src/App.js
@@ -2,17 +2,33 @@ import { useState } from 'react';
 import './App.css';
 import Header from './components/Header';
 import MainBoard from './components/MainBoard';
+require('dotenv').config()
 
 function App() {
   const [term, setTerm] = useState('');
 
-  let pins = [];
+  // let pins = [];
 
   const getSearchTerm = (searchWord) => {
-    // searchTerm = searchWord;
     setTerm(searchWord);
   };
+
   // axios call for unsplash api
+  const axios = require('axios');
+  // Make a get request to search photos
+  axios
+    .get(`https://api.unsplash.com/search/photos?client_id=${process.env.ACCESS_KEY}&query=${term}`)
+    .then(function (response) {
+      // handle success
+      console.log(response);
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+    .then(function () {
+      // always executed
+    });
 
   return (
     <div className="App">
